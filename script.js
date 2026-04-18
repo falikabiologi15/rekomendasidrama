f0eeb8ce5e33703a47b43e73f5fe10f19a475bc1
falikabiologi15 <falikabiologi15@gmail.com>
2026-04-18 22:07:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Website jalan!");
+function changeBg(image, title) {
+  const banner = document.querySelector('.banner');
+  const text = document.querySelector('.content h2');
+
+  banner.style.background =
+    "linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('" + image + "')";
+
+  text.innerHTML = title;
+}
