c472e43ce296825dcb3aa6de20b4cadbbcd4a048
falikabiologi15 <falikabiologi15@gmail.com>
2026-04-18 22:36:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Website jalan!");
-function changeBg(image, title) {
+function changeBg(image, title, el) {
   const banner = document.querySelector('.banner');
   const text = document.querySelector('.content h2');
 
@@ -7,4 +7,12 @@ function changeBg(image, title) {
     "linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('" + image + "')";
 
   text.innerHTML = title;
+
+  // hapus active semua
+  document.querySelectorAll('.movies img').forEach(img => {
+    img.classList.remove('active');
+  });
+
+  // kasih active ke yang diklik
+  el.classList.add('active');
 }
